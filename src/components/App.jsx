372ffa261f38c3a536ca93bbcf30129d6af34d97
Sergey Kabanets <skabanets97@gmail.com>
2024-01-29 23:48:38 +0200

    add login and register pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import { Phonebook } from './Phonebook/Phonebook';
 import { Layout } from './Layout/Layout';
 import { Home } from 'pages/Home/Home';
+import { Login } from 'pages/Login/Login';
+import { Register } from 'pages/Register/Register';
 
 export const App = () => {
   return (
@@ -9,6 +11,8 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="contacts" element={<Phonebook />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Route>
     </Routes>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
   return (
-    <div>
-      <h1>Home page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis iure ratione
-        necessitatibus facere dolorum, ut atque nemo quidem praesentium odit repellendus debitis
-        perferendis dolor nam itaque rem hic porro illo? Ad eligendi sequi maiores hic, itaque, a
-        repudiandae ratione aperiam, ipsum nobis repellat officiis aliquid! Aliquam porro, impedit
-        similique repellat repudiandae dolores consequuntur aliquid, officia atque eius nisi? Omnis,
-        necessitatibus? Odio ipsa in exercitationem soluta delectus iusto vitae nam! Placeat
-        quisquam quis cum omnis quibusdam nostrum! Aspernatur perspiciatis illum commodi asperiores
-        unde ullam obcaecati a id rem, sapiente, eum eius? Repellat consequuntur, beatae hic magni
-        exercitationem, voluptatibus atque odio fugiat laboriosam, accusantium vero quae dolores
-        nostrum officiis delectus omnis! Magnam nisi ipsum sed eaque rem unde fugit debitis earum
-        sit!
-      </p>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+            exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p>
+          <Link to="/contacts" className="btn btn-primary">
+            Get Started
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
